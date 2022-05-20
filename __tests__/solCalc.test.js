f7b30a475e4273cb11344d3ee5164bc4863302aa
Mauro Rosales Jr <mauro.rosales247@gmail.com>
2022-05-19 21:42:29 -0700

    pass all tests so far and refactor all the tests to add specific planet life spans

--- __tests__/solCalc.test.js
+++ __tests__/solCalc.test.js
@@ -20,20 +20,24 @@ describe('SolAge', () => {
     expect(myAge.earthDays(myAge.earthAge)).toEqual(365);
   });
 
-  test('should calculate the age of someone on murcury, using earth days', () => {
+  test('should calculate the age of someone on murcury and thier life expectancy, using earth days', () => {
     expect(myAge.mercuryYears(myAge.earthAge)).toEqual(153);
+    expect(myAge.mercuryYears(myAge.lifeSpan)).toEqual(302);
   });
 
-  test('should calculate the age of someone on Venus, using earthdays', () => {
+  test('should calculate the age of someone on Venus and thier life expectancy, using earthdays', () => {
     expect(myAge.venusYears(myAge.earthAge)).toEqual(60);
+    expect(myAge.venusYears(myAge.lifeSpan)).toEqual(118);
   })
 
-test('should calculate the age of someone on mars, using earth days', () => {
+test('should calculate the age of someone on mars and thier life expectancy, using earth days', () => {
   expect(myAge.marsYears(myAge.earthAge)).toEqual(19);
+  expect(myAge.mercuryYears(myAge.lifeSpan)).toEqual(38);
 })
 
-test('should calculate the age of someone on Jupiter, using earth days', () => {
+test('should calculate the age of someone on Jupiter and thier life expectancy, using earth days', () => {
   expect(myAge.jupiterYears(myAge.earthAge)).toEqual(3);
+  expect(myAge.mercuryYears(myAge.lifeSpan)).toEqual(6);
 })
 
 test('should calculate the age of someones life expectancy on mercury', () => {
